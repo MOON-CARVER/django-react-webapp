a435610cd20140ca6f6c01128b630ad3440bc980
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-08-05 21:21:57 +0530

    templates added
    room.html ,
    home.html ,
    developer.html .

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# django-studybuddy
+# django-studybuddy  
---
+++ base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request,'home.html')
+
+def room(request):
+    return render(request,'room.html')
+
+
+def developers(request):
+    return render(request,'developers.html')
