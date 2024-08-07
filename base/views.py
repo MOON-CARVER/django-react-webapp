68da840c41b354e59e3fe30b759060e62ded2172
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-08-07 20:51:34 +0530

    learning how to render data

--- base/views.py
+++ base/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+{'id' : 1, 'name':  'Lets learn Python!!!'},
+{'id' : 2, 'name':  'Design With Me'},
+{'id' : 3, 'name':  'Full Stack learning'},
+]
+
+
+
+
+
 
 def home(request):
-    return render(request,'home.html')
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
 
 def room(request):
     return render(request,'room.html')
