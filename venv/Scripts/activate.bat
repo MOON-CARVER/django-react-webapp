0495d366d59a29be5645000677a3fd4ce2751aca
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-09-16 17:29:34 +0530

    Revert "akash"
    This reverts commit c9609c259b7cb92496d4f726d80ca7756cd71911.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,51 +1,34 @@
-@REM This file is UTF-8 encoded, so we need to update the current code page while executing it
-@for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
-    @set _OLD_CODEPAGE=%%a
+@echo off
+
+rem This file is UTF-8 encoded, so we need to update the current code page while executing it
+for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
+    set _OLD_CODEPAGE=%%a
 )
-@if defined _OLD_CODEPAGE (
+if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-@set "VIRTUAL_ENV=D:\Assignment\Major Project-Sem 7\django-react-webapp\venv"
+set VIRTUAL_ENV=C:\Users\chand\OneDrive\Desktop\django-react-webapp\venv
 
-@set "VIRTUAL_ENV_PROMPT="
-@if NOT DEFINED VIRTUAL_ENV_PROMPT (
-    @for %%d in ("%VIRTUAL_ENV%") do @set "VIRTUAL_ENV_PROMPT=%%~nxd"
-)
+if not defined PROMPT set PROMPT=$P$G
 
-@if defined _OLD_VIRTUAL_PROMPT (
-    @set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
-) else (
-    @if not defined PROMPT (
-        @set "PROMPT=$P$G"
-    )
-    @if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-        @set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
-    )
-)
-@if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-    @set "PROMPT=(%VIRTUAL_ENV_PROMPT%) %PROMPT%"
-)
+if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
+if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-@REM Don't use () to avoid problems with them in %PATH%
-@if defined _OLD_VIRTUAL_PYTHONHOME @goto ENDIFVHOME
-    @set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
-:ENDIFVHOME
+set _OLD_VIRTUAL_PROMPT=%PROMPT%
+set PROMPT=(venv) %PROMPT%
 
-@set PYTHONHOME=
+if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
+set PYTHONHOME=
 
-@REM if defined _OLD_VIRTUAL_PATH (
-@if not defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH1
-    @set "PATH=%_OLD_VIRTUAL_PATH%"
-:ENDIFVPATH1
-@REM ) else (
-@if defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH2
-    @set "_OLD_VIRTUAL_PATH=%PATH%"
-:ENDIFVPATH2
+if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
+if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-@set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(venv) 
 
-@if defined _OLD_CODEPAGE (
+:END
+if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
-    @set _OLD_CODEPAGE=
+    set _OLD_CODEPAGE=
 )
