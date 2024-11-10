998866902c57609c54364cfa12e2539e95eb4d2b
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-11-10 21:52:03 +0530

    Participants tab added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Room,Topic 
+from .models import Room,Topic ,Message
 from .forms import RoomForm
 from django.db.models import Q 
 from django.contrib.auth.models import  User 
@@ -88,7 +88,20 @@ def home(request):
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created') #* we can query child objects of a specifc room  here 
-    context = {'room':room, 'room_messages' : room_messages}
+    participants = room.participants.all()
+    
+    if request.method == "POST":
+        message = Message.objects.create(
+          user = request.user ,
+          room = room ,
+          body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk = room.id)
+    
+    
+    
+    context = {'room':room, 'room_messages' : room_messages,'participants':participants}
     return render(request,'base/room.html',context)
 
 
