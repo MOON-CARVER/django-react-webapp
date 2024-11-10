f2d2766f3f57f2ca98323332f48affa61778ece0
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-11-10 21:20:13 +0530

    room message creation window added

--- base/views.py
+++ base/views.py
@@ -87,7 +87,8 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-created') #* we can query child objects of a specifc room  here 
+    context = {'room':room, 'room_messages' : room_messages}
     return render(request,'base/room.html',context)
 
 
