47ad1cf3f9387107d780fba2fda4cb7588e56eb7
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-11-13 15:07:16 +0530

    Deployment ready

--- django_react_webapp/settings.py
+++ django_react_webapp/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#+#lrm2+kwn*5#hy7-t2usy8_k$+p%o**oxv-n*avrl0mm*xyu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,7 +128,9 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+django_heroku.settings(locals())
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
--- venv/Lib/site-packages/django/db/migrations/operations/base.py
+++ venv/Lib/site-packages/django/db/migrations/operations/base.py
@@ -1,4 +1,15 @@
+import enum
+
 from django.db import router
+
+
+class OperationCategory(str, enum.Enum):
+    ADDITION = "+"
+    REMOVAL = "-"
+    ALTERATION = "~"
+    PYTHON = "p"
+    SQL = "s"
+    MIXED = "?"
 
 
 class Operation:
@@ -33,6 +44,8 @@ class Operation:
 
     serialization_expand_args = []
 
+    category = None
+
     def __new__(cls, *args, **kwargs):
         # We capture the arguments to make returning them trivial
         self = object.__new__(cls)
@@ -56,14 +69,18 @@ class Operation:
         Take the state from the previous migration, and mutate it
         so that it matches what this migration would perform.
         """
-        raise NotImplementedError('subclasses of Operation must provide a state_forwards() method')
+        raise NotImplementedError(
+            "subclasses of Operation must provide a state_forwards() method"
+        )
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         """
         Perform the mutation on the database schema in the normal
         (forwards) direction.
         """
-        raise NotImplementedError('subclasses of Operation must provide a database_forwards() method')
+        raise NotImplementedError(
+            "subclasses of Operation must provide a database_forwards() method"
+        )
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
         """
@@ -71,13 +88,22 @@ class Operation:
         direction - e.g. if this were CreateModel, it would in fact
         drop the model's table.
         """
-        raise NotImplementedError('subclasses of Operation must provide a database_backwards() method')
+        raise NotImplementedError(
+            "subclasses of Operation must provide a database_backwards() method"
+        )
 
     def describe(self):
         """
         Output a brief summary of what the action does.
         """
         return "%s: %s" % (self.__class__.__name__, self._constructor_args)
+
+    def formatted_description(self):
+        """Output a description prefixed by a category symbol."""
+        description = self.describe()
+        if self.category is None:
+            return f"{OperationCategory.MIXED.value} {description}"
+        return f"{self.category.value} {description}"
 
     @property
     def migration_name_fragment(self):
