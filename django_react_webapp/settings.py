9b8a559430f0b41873bd3e007f5cdd4a3e3c563f
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-11-13 15:52:49 +0530

    idk

--- django_react_webapp/settings.py
+++ django_react_webapp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'whitenoise.runserver_nostatic',
     'base.apps.BaseConfig',
 ]
 
@@ -47,8 +47,7 @@ AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
