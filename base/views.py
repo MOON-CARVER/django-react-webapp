70029bbbdc9e05262374093c7e24dd659aa5e1f9
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-11-13 10:47:30 +0530

    update user complete

--- base/views.py
+++ base/views.py
@@ -190,5 +190,10 @@ def updateUser(request):
     user = request.user
     form = UserForm(instance = request.user)
     
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid:
+            form.save()
+            return redirect('user-profile',pk = user.id)
     
     return render(request, 'base/update-user.html',{'form':form })
